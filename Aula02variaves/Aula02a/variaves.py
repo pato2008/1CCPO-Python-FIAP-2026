afe2c593d09af37939ca1a8b856f3078d06b6a99
pato2008 <jpcostas557@gmail.com>
2026-03-19 20:57:08 -0300

    Aula 2 variaves e operadoes desafios

---
+++ Aula02variaves/Aula02a/variaves.py
@@ -0,0 +1,37 @@
+print("hola mundo")
+
+print(7 + 4) #soma
+
+print("7+4")#texto
+
+print("7"+'4')
+
+'''
+comentarios 
+varias
+linguas
+rezenha
+'''
+
+#variaves
+nome = "joao pedro" #str
+idade = 18 #int
+peso = 109 #float
+print(nome, idade, peso)
+print(f"salve , {nome}!!!")
+
+#input - simulação de forms no cmd
+
+nome = input("Digite o seu nome: ")
+idade =int(input(" digire sua idade:"))
+peso =float (input(" digire seu peso:"))
+print(nome, idade, peso,)
+print(nome)
+
+ano_nacimento = 1999
+ano_atual =2026
+idade = ano_atual - ano_nacimento
+print(f"sua idade é: {idade}")
+
+
+
---
+++ desafios/desafios.py
@@ -0,0 +1,7 @@
+'''
+#Desafio 1
+nome = input("Digite o seu nome: ")
+print(f"bem vindo , {nome}!!!")
+'''
+#desafio 2
+conta =
